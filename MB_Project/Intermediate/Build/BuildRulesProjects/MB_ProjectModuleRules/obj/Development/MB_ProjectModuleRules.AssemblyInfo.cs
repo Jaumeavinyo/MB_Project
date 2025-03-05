a972981f602c73f4d786b0d8da171932a0fd98d6
jas3 <36154250+Jaumeavinyo@users.noreply.github.com>
2025-03-05 13:54:02 +0100

    movement first version done

--- MB_Project/Intermediate/Build/BuildRulesProjects/MB_ProjectModuleRules/obj/Development/MB_ProjectModuleRules.AssemblyInfo.cs
+++ MB_Project/Intermediate/Build/BuildRulesProjects/MB_ProjectModuleRules/obj/Development/MB_ProjectModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb296b3e6e1eb68f930833369a4fcf38693d201a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ddb5ac3f396dd931e9e46f132ddf89f25011976")]
 [assembly: System.Reflection.AssemblyProductAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
