167e5caee51681ce155b644560034d5c42821754
THchete <nachofernandezlopezz@gmail.com>
2025-03-14 17:35:45 +0100

    cosas q casi puierdo por reiniaciar el pc

--- MB_Project/Intermediate/Build/BuildRulesProjects/MB_ProjectModuleRules/obj/Development/MB_ProjectModuleRules.AssemblyInfo.cs
+++ MB_Project/Intermediate/Build/BuildRulesProjects/MB_ProjectModuleRules/obj/Development/MB_ProjectModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8854b2f23466d4554b9edb764fbb37beb3bc252d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d640c7e0db54dba9d6d94b99a7acbe820cfd068")]
 [assembly: System.Reflection.AssemblyProductAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
