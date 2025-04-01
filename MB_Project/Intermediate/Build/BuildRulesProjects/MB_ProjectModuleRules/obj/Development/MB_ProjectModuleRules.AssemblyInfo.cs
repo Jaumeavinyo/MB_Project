394476852841fcc01ff40cd99491851be9b4148d
jas3 <36154250+Jaumeavinyo@users.noreply.github.com>
2025-04-01 21:24:26 +0200

    Nueva version de BP logica de la camara estable y añadidas 3 camaras nuevas

--- MB_Project/Intermediate/Build/BuildRulesProjects/MB_ProjectModuleRules/obj/Development/MB_ProjectModuleRules.AssemblyInfo.cs
+++ MB_Project/Intermediate/Build/BuildRulesProjects/MB_ProjectModuleRules/obj/Development/MB_ProjectModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+649d37162eceaada111edc0852abb98a6801d84c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86811be0e04e2c282a84135dfc78734ebc674cf9")]
 [assembly: System.Reflection.AssemblyProductAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
