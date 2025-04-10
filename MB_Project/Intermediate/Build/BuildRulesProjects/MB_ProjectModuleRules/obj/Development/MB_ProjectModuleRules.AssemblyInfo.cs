89628012aac2b3de823bb6f67a16c80093107d6d
jas3 <36154250+Jaumeavinyo@users.noreply.github.com>
2025-04-10 19:02:40 +0200

    Revert "asdfa"
    This reverts commit ea59411a276b7e1a81aab4356c854089eea6b7e6.

--- MB_Project/Intermediate/Build/BuildRulesProjects/MB_ProjectModuleRules/obj/Development/MB_ProjectModuleRules.AssemblyInfo.cs
+++ MB_Project/Intermediate/Build/BuildRulesProjects/MB_ProjectModuleRules/obj/Development/MB_ProjectModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a50fd94cf4a144925aadc139a384a34318f0dd26")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+649d37162eceaada111edc0852abb98a6801d84c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
