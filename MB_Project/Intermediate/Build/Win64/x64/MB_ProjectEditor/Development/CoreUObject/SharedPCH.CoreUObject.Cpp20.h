684633241f31a4b334a25887b280799b6b367e4a
LiaValverde <162477791+LiaValverde@users.noreply.github.com>
2025-04-23 10:07:00 +0200

    Relighting: skybox and light sphere
    Removed default skybox and created a new one, I also created the glowing sphere for illumination.

--- MB_Project/Intermediate/Build/Win64/x64/MB_ProjectEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
+++ MB_Project/Intermediate/Build/Win64/x64/MB_ProjectEditor/Development/CoreUObject/SharedPCH.CoreUObject.Cpp20.h
@@ -1,3 +1,3 @@
 // PCH for Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h
-#include "C:/Users/jauma/Documents/GitHub/MistBornUE5/Misborn_Prototype/MB_Project/MB_Project/Intermediate/Build/Win64/x64/MB_ProjectEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
+#include "C:/Users/Husband/Documents/GitHub/MB_Project/MB_Project/Intermediate/Build/Win64/x64/MB_ProjectEditor/Development/CoreUObject/SharedDefinitions.CoreUObject.Cpp20.h"
 #include "Runtime/CoreUObject/Public/CoreUObjectSharedPCH.h"
