885e5e3c257ed4478cc7d424e82e32aed9c3a505
jas3 <36154250+Jaumeavinyo@users.noreply.github.com>
2025-10-14 21:48:00 +0200

    lots of MM

--- MB_Project/Intermediate/Build/BuildRulesProjects/MB_ProjectModuleRules/obj/Development/MB_ProjectModuleRules.AssemblyInfo.cs
+++ MB_Project/Intermediate/Build/BuildRulesProjects/MB_ProjectModuleRules/obj/Development/MB_ProjectModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e91fd944f4f2bf158ea9b691d523f3c20bd032df")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fc17e8606b320b13a6268882bffd17b31ecbc8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
