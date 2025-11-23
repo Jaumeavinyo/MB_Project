9353be878166c481a950391641638b9f2315b0e4
jas3 <36154250+Jaumeavinyo@users.noreply.github.com>
2025-11-24 00:11:50 +0100

    character leans when pulling

--- MB_Project/Intermediate/Build/BuildRulesProjects/MB_ProjectModuleRules/obj/Development/MB_ProjectModuleRules.AssemblyInfo.cs
+++ MB_Project/Intermediate/Build/BuildRulesProjects/MB_ProjectModuleRules/obj/Development/MB_ProjectModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+830823fc7cf9d1bf96f6d29fc6fd17fed9292889")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02bac3a375a417a8ff3e72f2fb1dd73986e65c09")]
 [assembly: System.Reflection.AssemblyProductAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MB_ProjectModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
